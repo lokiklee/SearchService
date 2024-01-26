9bac4ff84b03bf8a271a783634409e1d09c2ca7a
WanrongSun <maggie385277433@hotmail.com>
2024-01-25 19:30:37 -0800

    Add response header

--- src/main/java/integration/ninjas/HandlerString.java
+++ src/main/java/integration/ninjas/HandlerString.java
@@ -1,17 +1,22 @@
 package integration.ninjas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HandlerString implements RequestHandler<String, String>{
+public class HandlerString implements RequestHandler<String, Response> {
 
     @Override
-    public String handleRequest(String input, Context context)
-    {
+    public Response handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log("Input TYPE: " + input.getClass().toString());
         logger.log("Input Value: " + input);
-        return new StringBuilder(input).reverse().toString();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+
+        return new Response(200, headers, new StringBuilder(input).reverse().toString());
     }
 }
